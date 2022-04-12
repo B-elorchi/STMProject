e647b55b43f21a0ecdf7b17edd38fc3b06398bfa
Brahim Elorchi <brahimelorchi1937@gmail.com>
2022-04-12 10:15:22 +0000

    complete design home page

---
+++ js/script.js
@@ -0,0 +1,30 @@
+$(document).ready(() => {
+	// JQuery Code
+	// Start Section Header
+});
+
+let x;
+let widthW = window.innerWidth;
+if (widthW <= 600) {
+	x = 1;
+	console.log(window.innerWidth);
+} else if (widthW >= 768 && widthW < 992) {
+	x = 2;
+	console.log(window.innerWidth);
+} else if (widthW > 992) {
+	x = 3;
+	console.log(window.innerWidth);
+}
+new Splide(".splide", {
+	type: "loop",
+	autoplay: "play",
+	perPage: x,
+}).mount();
+if(window.scrollY >107){
+    console.log('yes');
+    console.log(window.scrollY);
+}else{
+    console.log("yes");
+}
+
+
